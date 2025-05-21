d4f3d8370513102192126b5e59f5da792366c091
Lakhan Soni <lakhan.soni@eroam.com>
2025-05-21 22:34:59 +0530

    deploy Ecommerce project

--- Client/api/siteConfig.js
+++ Client/api/siteConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
-  baseURL:  import.meta.env.VITE_BASE_URL || 'http://localhost:5000/api',
+  baseURL:  import.meta.env.VITE_BASE_URL || 'https://ecommerce-aadw.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
