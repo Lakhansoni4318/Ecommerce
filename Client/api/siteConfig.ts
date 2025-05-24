5cb1173c5be7683732415b8c98619eafa717cf0f
Lakhan Soni <lakhan.soni@eroam.com>
2025-05-25 01:41:34 +0530

    deploy Ecommerce project

--- Client/api/siteConfig.ts
+++ Client/api/siteConfig.ts
@@ -1,17 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL:  'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:5000/api",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
   timeout: 5000,
 });
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
     return config;
   },
